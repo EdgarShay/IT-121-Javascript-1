9f7227aedcc20f6963696da01c58398234efe34b
EdgarShay <97201888+EdgarShay@users.noreply.github.com>
2023-04-16 22:31:03 -0700

    Add files via upload

---
+++ week_2/ch2_let_variables_using.js
@@ -0,0 +1,20 @@
+let str1 = 'Laurence'; 
+let str2 = "Svekis"; 
+let val1 = undefined;
+let val2 = null;
+let myNum = 1000;
+
+// string 1
+console.log("typeof str1"); 
+
+// string 2
+console.log("typeof str2"); 
+
+// undefined value
+console.log("typeof val1"); 
+
+// object (null is an object in JavaScript value) 
+console.log("typeof val2"); 
+
+ // number
+console.log("typeof myNum"); 
